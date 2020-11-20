fe66f53f8dd5420a1f7f5682fa113d96c3d5ce40
Thomas Brannan <th.c.brannan@gmail.com>
2020-11-20 14:55:38 -0800

    Very rough modal appears and renders images.

---
+++ client/src/components/ModalCarousel.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import $ from 'jquery'; // for ajax
+import styled from 'styled-components';
+import { ModalCarouselStyle, PhotosBoxParent, ColPhotosBox, BigPhoto, LilPhoto, ModalCarouselContainer } from './styledComponents.jsx'
+
+const displayBlock = styled
+
+const ModalCarousel = (props) => {
+  return(
+    <ModalCarouselContainer>
+
+    </ModalCarouselContainer>
+  );
+};
+
+export default ModalCarousel;
+
