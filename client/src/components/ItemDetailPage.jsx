1c8a7620f5db1fb9c91eeda35ad8b450545a684b
Thomas Brannan <th.c.brannan@gmail.com>
2020-11-25 00:21:16 -0800

    Radiused corners on the item detail page

--- client/src/components/ItemDetailPage.jsx
+++ client/src/components/ItemDetailPage.jsx
@@ -3,34 +3,45 @@ import $ from 'jquery'; // for ajax
 import styled from 'styled-components';
 
 const PhotosBoxParent = styled.div`
-display: flex;
-justify-content: center;
+  display: flex;
+  justify-content: center;
 `;
 
 const ColPhotosBox = styled.div`
-display: flex;
-flex-direction: column;
+  display: flex;
+  flex-direction: column;
 `;
 
 const BigPhoto = styled.img`
-height: 500px;
-width: 744px;
-padding: 10px;
+  height: 500px;
+  width: 744px;
+  padding: 10px;
+  border-top-left-radius: 25px;
+  border-bottom-left-radius: 25px;
 `;
 
-const LilPhoto = styled.img`
-height: 250px;
-width: 248px;
-padding: 5px;
+const LilPhoto_upper = styled.img`
+  height: 250px;
+  width: 248px;
+  padding: 5px;
+  border-top-right-radius: 25px;
 `;
+
+const LilPhoto_lower = styled.img`
+  height: 250px;
+  width: 248px;
+  padding: 5px;
+  border-bottom-right-radius: 25px;
+`;
+
 
 const ItemDetailPage = (props) => {
   return (
     <PhotosBoxParent>
       <BigPhoto src={props.photos.large[0]}></BigPhoto>
       <ColPhotosBox>
-        <LilPhoto src={props.photos.small[0]}></LilPhoto>
-        <LilPhoto src={props.photos.small[1]}></LilPhoto>
+        <LilPhoto_upper src={props.photos.small[0]}></LilPhoto_upper>
+        <LilPhoto_lower src={props.photos.small[1]}></LilPhoto_lower>
       </ColPhotosBox>
     </PhotosBoxParent>
   );
