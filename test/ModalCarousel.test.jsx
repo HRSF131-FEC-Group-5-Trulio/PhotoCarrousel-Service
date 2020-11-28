8f09b90b8a3900a502858680a728d3ac2eb6d2e4
Thomas Brannan <th.c.brannan@gmail.com>
2020-11-27 17:13:36 -0800

    Prepared envrionment for testing react with jest-enzyme

---
+++ test/ModalCarousel.test.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import ModalCarousel from "../client/src/components/ModalCarousel.jsx";
+
+describe("Hello World", () => {
+  it("true is true", () => {
+    expect(true).toEqual(true);
+  });
+});
